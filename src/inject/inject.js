41bb7d5e5ce91eb16130977b085fc2e9704394e2
Jade Allen Cook <jadeallencook@gmail.com>
2016-08-17 22:05:40 -0400

    ⚗ customized build

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,8 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		console.log('script is running...');
 	}
 	}, 10);
 });
